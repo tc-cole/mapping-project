629290c52702a818ba00c052380dd61e2c4a10d9
tccole <tccoleman26@gmail.com>
2025-04-24 09:33:59 -0400

    updating navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import '../app.css';
+
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import AppSidebar from "$lib/components/navigation/app-sidebar.svelte";
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Sidebar.Provider>
+	<AppSidebar />
+	<main>
+	  <Sidebar.Trigger />
+	  {@render children?.()}
+	</main>
+  </Sidebar.Provider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
     import { Button } from "$lib/components/ui/button/index.js";
-   </script>
+</script>
     
-   <Button>Click me</Button>
