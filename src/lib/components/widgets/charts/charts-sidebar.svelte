2d42429272f568e7c5ec5b06f21fd2feb885fd42
tccole <tccoleman26@gmail.com>
2025-05-19 15:38:49 -0400

    updating all of this

--- src/lib/components/widgets/charts/charts-sidebar.svelte
+++ src/lib/components/widgets/charts/charts-sidebar.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
 	import { Drawer } from 'vaul-svelte';
 	import { openDrawer } from '../menu/DrawingTools.svelte';
+	import * as Chart from '$lib/components/ui/chart/index.js';
+
+	const chartData = [
+		{ month: 'January', desktop: 186, mobile: 80 },
+		{ month: 'February', desktop: 305, mobile: 200 },
+		{ month: 'March', desktop: 237, mobile: 120 },
+		{ month: 'April', desktop: 73, mobile: 190 },
+		{ month: 'May', desktop: 209, mobile: 130 },
+		{ month: 'June', desktop: 214, mobile: 140 }
+	];
+
+	const chartConfig = {
+		desktop: {
+			label: 'Desktop',
+			color: '#2563eb'
+		},
+		mobile: {
+			label: 'Mobile',
+			color: '#60a5fa'
+		}
+	} satisfies Chart.ChartConfig;
 </script>
 
 <Drawer.Root direction="right" open={$openDrawer}>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,6 @@
 import { fontFamily } from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 import tailwindcssAnimate from 'tailwindcss-animate';
-const colors = require('tailwindcss/colors');
-const layerstack = require('@layerstack/tailwind/plugin');
 
 const config: Config = {
 	darkMode: ['class'],
@@ -11,30 +9,6 @@ const config: Config = {
 		'./node_modules/svelte-ux/**/*.{svelte,js}',
 		'./node_modules/layerchart/**/*.{svelte,js}' // <--- Add this
 	],
-	ux: {
-		themes: {
-			light: {
-				primary: colors['orange']['500'],
-				'primary-content': 'white',
-				secondary: colors['blue']['500'],
-				'surface-100': 'white',
-				'surface-200': colors['gray']['100'],
-				'surface-300': colors['gray']['300'],
-				'surface-content': colors['gray']['900'],
-				'color-scheme': 'light'
-			},
-			dark: {
-				primary: colors['orange']['500'],
-				'primary-content': 'white',
-				secondary: colors['blue']['500'],
-				'surface-100': colors['zinc']['800'],
-				'surface-200': colors['zinc']['900'],
-				'surface-300': colors['zinc']['950'],
-				'surface-content': colors['zinc']['100'],
-				'color-scheme': 'dark'
-			}
-		}
-	},
 	safelist: ['dark'],
 	theme: {
 		container: {
@@ -120,7 +94,7 @@ const config: Config = {
 			}
 		}
 	},
-	plugins: [tailwindcssAnimate, layerstack]
+	plugins: [tailwindcssAnimate]
 };
 
 export default config;
