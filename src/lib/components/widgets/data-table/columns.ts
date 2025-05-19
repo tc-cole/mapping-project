a24f3c99eeaa4952478290a2f9198d4db97ed302
tccole <tccoleman26@gmail.com>
2025-05-19 12:35:29 -0400

    udpating widgets

---
+++ src/lib/components/widgets/data-table/columns.ts
@@ -0,0 +1,89 @@
+import { renderComponent } from '$lib/components/ui/data-table/index.js';
+import DataTableSortable from './data-table-sortable.svelte';
+// bunimport DataTableActions from './data-table-actions.svelte';
+
+function formatDate(value: any): string {
+	return new Date(value).toLocaleDateString();
+}
+
+function formatNumber(value: any): string {
+	return value.toString();
+}
+
+function formatObject(value: any): string {
+	return JSON.stringify(value);
+}
+
+function capitalize(str: string): string {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+type Schema<Key extends string, T> = {
+	columnName: Key;
+	type: T;
+};
+
+type ColumnTypes = 'string' | 'number' | 'date' | 'object';
+type DatabaseSchema = Schema<string, ColumnTypes>[];
+
+export function createTableColumns(schema: DatabaseSchema) {
+	const columns = [
+		{
+			id: 'select',
+			enableSorting: false,
+			enableHiding: false
+		}
+	];
+
+	// Add dynamic data columns based on schema
+	schema.forEach((column) => {
+		const { columnName, type } = column;
+
+		//if (['select', 'actions'].includes(columnName)) {
+		//	return;
+		//}
+
+		let columnDef = {
+			accessorKey: columnName, //@ts-expect-error
+			header: ({ column }) =>
+				renderComponent(DataTableSortable, {
+					onclick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+					innerText: capitalize(columnName), // Display the capitalized column name
+					'aria-label': `Sort by ${columnName}`,
+					variant: 'ghost',
+					columnName
+				}),
+			cell: ({ row }: any) => {
+				const value = row.getValue(columnName);
+
+				// Customize cell rendering based on type
+				switch (type) {
+					case 'string':
+						return value;
+					case 'number':
+						return formatNumber(value);
+					case 'date':
+						return formatDate(value);
+					case 'object':
+						return formatObject(value);
+					default:
+						return value;
+				}
+			}
+		};
+
+		//@ts-expect-error
+		columns.push(columnDef);
+	});
+
+	// Actions column
+
+	//columns.push({
+	//	id: 'actions', //@ts-expect-error
+	//	cell: ({ row }) => {
+	//		return renderComponent(DataTableActions, { id: row.original.id });
+	//	}
+	//});
+
+	return columns;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	import AppSidebar from '$lib/components/widgets/nav-sidebar/navigation-sidebar.svelte';
 	import DeckGL from '$lib/components/DeckGL/DeckGL.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import ChartsSidebar from '$lib/components/widgets/charts/charts-sidebar.svelte';
-	import DrawingTools from '$lib/components/widgets/menu/DrawingTools.svelte';
+	//import ChartsSidebar from '$lib/components/widgets/charts/charts-sidebar.svelte';
+	//import DrawingTools from '$lib/components/widgets/menu/DrawingTools.svelte';
 </script>
 
 <Sidebar.Provider>
