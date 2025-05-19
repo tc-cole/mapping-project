ba09d05f8781e477c2512a5a5893cce0e80ce866
tccole <tccoleman26@gmail.com>
2025-05-19 16:01:25 -0400

    looks good

---
+++ src/lib/components/widgets/charts/charts-drawer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Drawer } from 'vaul-svelte';
+	import { openDrawer } from '../menu/DrawingTools.svelte';
+	//import * as Chart from '$lib/components/ui/chart/index.js';
+
+	import BarChart from './charts/bar-chart.svelte';
+</script>
+
+<Drawer.Root direction="right" open={$openDrawer}>
+	<Drawer.Portal>
+		<Drawer.Content
+			class="fixed bottom-0 right-0 top-0 flex h-full w-2/5 flex-col bg-zinc-800 shadow-lg"
+			onEscapeKeydown={(e) => {
+				e.preventDefault();
+				openDrawer.set(false);
+			}}
+			onInteractOutside={(e) => {
+				e.preventDefault();
+				openDrawer.set(false);
+			}}
+		>
+			<BarChart />
+		</Drawer.Content>
+	</Drawer.Portal>
+</Drawer.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
 	import AppSidebar from '$lib/components/widgets/nav-sidebar/navigation-sidebar.svelte';
 	import DeckGL from '$lib/components/DeckGL/DeckGL.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import ChartsSidebar from '$lib/components/widgets/charts/charts-sidebar.svelte';
+	import ChartsDrawer from '$lib/components/widgets/charts/charts-drawer.svelte';
 	//import DrawingTools from '$lib/components/widgets/menu/DrawingTools.svelte';
 </script>
 
 <Sidebar.Provider>
 	<AppSidebar />
 
-	<ChartsSidebar />
+	<ChartsDrawer />
 </Sidebar.Provider>
 
 <DeckGL />
